0568b0431624c4d7f0ee366fb844b2cc62a4747d
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-12-05 01:08:44 -0300

    wip: material_exame entity

---
+++ src/main/java/br/com/reportorm/entities/MaterialExame.java
@@ -0,0 +1,76 @@
+package br.com.reportorm.entities;
+
+import java.io.Serializable;
+
+public class MaterialExame implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String material;
+    private String observacao;
+    
+    public MaterialExame() {
+    }
+
+    public MaterialExame(Integer id, String material, String observacao) {
+        this.id = id;
+        this.material = material;
+        this.observacao = observacao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    @Override
+    public String toString() {
+        return "MaterialExame [id=" + id + ", material=" + material + ", observacao=" + observacao + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MaterialExame other = (MaterialExame) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+    
+}
